a3ee7181b76bfe30f464eb74a55db9db638c7ece
gorki-unicorn <58412153+gorki-unicorn@users.noreply.github.com>
2019-12-04 13:04:36 +0600

    Update Dbmanager.java

--- app/src/main/java/com/example/sqlitedemoapp/Dbmanager.java
+++ app/src/main/java/com/example/sqlitedemoapp/Dbmanager.java
@@ -1,54 +1,69 @@
 package com.example.sqlitedemoapp;
-
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.jar.Attributes;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+
 public class Dbmanager extends SQLiteOpenHelper {
-    public static  final String dbname= "uddipandb";
-    public static final String TABLE_NAME = "tbl_eminfo";
+
+    public static final String DATABASE_NAME = "Employee.db";
+    public static final String TABLE_NAME = "employee_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "DEPT_NAME";
+    public static final String COL_4 = "PIN";
+
+   // public static final String KEY_ID = "id"; // eash user has unique id
+   // public static final String KEY_PIN = "pin";
+   // public static final String KEY_NAME = "name";
+   // public static final String KEY_DEPT = "dept";
+
+
+    //private static  final int version = 1;
     public Dbmanager(Context context)
     {
-        super(context, dbname, null, 1);
-        //SQLiteDatabase db= this.getWritableDatabase();
+        super(context, DATABASE_NAME, null, 1);
     }
-
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-       String qry="create table " +TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, PIN TEXT, NAME TEXT, DEPT NAME)";
-       db.execSQL(qry);
-
-
+        // String sql="create table employee (id integer primary key autoincrement,pin text,name text, dept text)";
+        db.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,DEPT_NAME TEXT,PIN INTEGER)");
+        // db.execSQL(sql);
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-     db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME );
-     onCreate(db);
-
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
     }
-
-    public String addRecord(String p1,String p2, String p3)
+    public boolean insertData(String name,String DEPT_NAME,String PIN)
     {
-
-        SQLiteDatabase db=this.getWritableDatabase();
-
-        ContentValues cv=new ContentValues();
-        cv.put("pin",p1);
-        cv.put("name",p2);
-        cv.put("dept",p3);
-
-        long res=db.insert("tbl_empinfo",null,cv);
-
-        if(res==-1)
-            return  "Failed";
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,DEPT_NAME);
+        contentValues.put(COL_4,PIN);
+        long result = db.insert(TABLE_NAME,null ,contentValues);
+        if(result == -1)
+            return false;
         else
-            return "Sucessfully Inserted";
+            return true;
 
     }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
+        return res;
+    }
+
+
 }
+
