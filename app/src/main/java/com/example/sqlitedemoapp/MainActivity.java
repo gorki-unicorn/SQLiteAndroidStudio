cbbf3ce44a3477380cc1c29c4d3e79c0d47764b8
gorki-unicorn <58412153+gorki-unicorn@users.noreply.github.com>
2019-12-04 13:05:01 +0600

    Update MainActivity.java

--- app/src/main/java/com/example/sqlitedemoapp/MainActivity.java
+++ app/src/main/java/com/example/sqlitedemoapp/MainActivity.java
@@ -4,22 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b = (Button) findViewById(R.id.clkme);
+        b.setOnClickListener(new View.OnClickListener()  {
 
-    }
+            @Override
+            public void onClick (View v)
 
-    public void startdbapp (View view)
+            {
 
-    {
-     new Dbmanager(this);
-     startActivity(new Intent(this,InsertData.class));
+                Toast.makeText(getApplicationContext(),"",Toast.LENGTH_LONG).show();
+                Intent i =new Intent(MainActivity.this, InsertData.class);
+                startActivity(i);
+                // startActivities(i);
 
+            }
+
+        });
     }
 }
